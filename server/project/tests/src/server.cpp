8743f4385f771f5d98bef51ff77cf80665fb365c
andrey brazhnikov <andreybr02@yandex.ru>
2021-04-15 18:18:35 +0300

    add tests

--- server/project/tests/src/server.cpp
+++ server/project/tests/src/server.cpp
@@ -1,15 +1,14 @@
 #include "tools.h"
 #include "server.h"
 
-TEST(Server, Ordinary) {
-    network::Server server;
-    server.
-}
-
-TEST(ParallelSort, Stress) {
-
-}
-
-TEST(ParallelSort, Broken) {
-
+TEST(Server, User) {
+    boost::asio::io_context context;
+    network::User user(context);
+    boost::asio::streambuf buffer;
+    std::ostream os(&buffer);
+    os << "msg";
+    ASSERT_TRUE(user.Connect());
+    ASSERT_EQ(user.Send(buffer), 0);
+    ASSERT_EQ(user.Read(buffer), 0);
+    ASSERT_TRUE(user.Disconnect());
 }
