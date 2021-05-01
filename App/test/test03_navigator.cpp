ce7e4348151062ff07e33fea9b7bbf89cbe571df
Alexander Klonov <alxender.klonov@mail.ru>
2021-05-02 00:39:50 +0300

    Tweaked test 3

--- App/test/test03_navigator.cpp
+++ App/test/test03_navigator.cpp
@@ -3,11 +3,17 @@
 
 #include "navigator.h"
 #include "screenfactory.h"
+
+#include <QApplication>
 using namespace screens;
 
 TEST(Navigator, test1) {
-    QStackedWidget *container = nullptr;
-    BaseScreensFactory *screensFactory = nullptr;
+    int argc = 1;
+    char *argv[0];
+    QApplication a(argc, argv);
+
+    QStackedWidget *container = new QStackedWidget;
+    BaseScreensFactory *screensFactory = new ScreensFactory;
     auto nav = new FragmentNavigator(container, screensFactory);
     ASSERT_NO_THROW(nav->navigateTo(LOGIN_TAG));
     ASSERT_NO_THROW(nav->back());
